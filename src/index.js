35717d9a28a0229451635574dcb7e68f46b63663
Mostafa Abdelbari <mmoabdelbari@gmail.com>
2023-11-13 12:05:52 -0800

    Latests Update

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min'
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'react-toastify/dist/ReactToastify.min.css';
 import 'react-lazy-load-image-component/src/effects/blur.css';
